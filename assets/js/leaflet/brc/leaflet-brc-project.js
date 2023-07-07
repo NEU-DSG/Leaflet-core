cf141ed21ecd466b3365702410fc603b981e1273
aravaraghu <97917143+aravaraghu@users.noreply.github.com>
2023-07-07 16:10:48 -0400

    Moved the hardcoded parts and constants to a saperate js file.

--- assets/js/leaflet/brc/leaflet-brc-project.js
+++ assets/js/leaflet/brc/leaflet-brc-project.js
@@ -1,67 +1,13 @@
 jQuery(function() {
-
 var map = L.map('map', {
-    center: [0, 0],
-    zoom: 5,
-    scrollWheelZoom: true
+    center: configStoryMap.center,
+    zoom: configStoryMap.zoom,
+    scrollWheelZoom: configStoryMap.scrollWheelZoom
 });
 
-var tileLayer = new L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+var tileLayer = new L.tileLayer(configStoryMap.titleLayerMap, {
+    attribution: configStoryMap.titleLayerAttribution
 }).addTo(map);
-
-// This customizes link to view source code; add your own GitHub repository
-map.attributionControl
-    .setPrefix('View <a href="http://github.com/jackdougherty/leaflet-storymap" target="_blank">code on GitHub</a>, created with <a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>');
-
-
-
-// Sparql Query used to get the data from the wikidata site, inorder to fetch the data dynamically
-// this query can be used to get the most updated data, this query is based on the json fields. 
-const sparqlQuery = `select distinct ?work ?workDescription ?workLabel ?coords ?address
-(group_concat(distinct ?workAlias; separator="; ") as ?aliases)
-(sample(?image) AS ?image) 
-(sample(?DRSImageURL) AS ?DRSImageURL)
-(group_concat(distinct (year(?dateInstalled)); separator=" or ") as ?yearInstalled)
-(group_concat(distinct (year(?dateRemoved)); separator=" or ") as ?yearRemoved)
-(group_concat(distinct ?materialLabel; separator="; ") as ?materials)
-(group_concat(distinct ?categoryLabel; separator="; ") as ?categories)
-(group_concat(distinct ?creatorLabel; separator="; ") as ?creators)
-(group_concat(distinct ?neighborhoodLabel; separator="; ") as ?neighborhoods)
-(group_concat(distinct ?depictsLabel; separator="; ") as ?depicted)
-(group_concat(distinct ?commemoratesLabel; separator="; ") as ?commemorated)
-where {
-    hint:Query hint:optimizer "None".
-    # Items tagged as being on the focus list of the Neighborhood Public Art in Boston WikiProject
-    # and with genre public art. Only grab items that have statements for coordinate location, instance of, 
-    # located in administrative territorial entity properties
-    ?work wdt:P5008 wd:Q108040915;
-        wdt:P136 wd:Q557141;
-        wdt:P625 ?coords;
-        wdt:P31 ?category;
-        wdt:P131 ?neighborhood.
-    optional{?work wdt:P571 ?dateInstalled.}
-    optional{?work wdt:P576 ?dateRemoved.}
-    optional{?work wdt:P18 ?image.}
-    optional{?work wdt:P170 ?creator.}
-    optional{?work wdt:P6375 ?address.}
-    optional{?work wdt:P186 ?material.}
-    optional{?work wdt:P180 ?depicts.}
-    optional{?work wdt:P547 ?commemorates.}
-    optional{?work wdt:P6500 ?DRSImageURL.
-            FILTER(regex(str(?DRSImageURL), '^https://repository.library.northeastern.edu/'))}
-    service wikibase:label {bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". 
-                            ?work rdfs:label ?workLabel.
-                            ?work schema:description ?workDescription.
-                            ?work skos:altLabel ?workAlias.
-                            ?category rdfs:label ?categoryLabel.
-                            ?creator rdfs:label ?creatorLabel.
-                            ?material rdfs:label ?materialLabel.
-                            ?neighborhood rdfs:label ?neighborhoodLabel.
-                            ?depicts rdfs:label ?depictsLabel.
-                            ?commemorates rdfs:label ?commemoratesLabel.
-                            }
-    } group by ?work ?workDescription ?workLabel ?coords ?address`
 
 // axios.get('https://query.wikidata.org/sparql', {
 //     params: {
@@ -79,10 +25,10 @@ where {
 //   });
 
 // Fetch api request for getting the bindings from the wikidata website.
-url = new URL("https://query.wikidata.org/sparql?format=json&")
+var url = new URL("https://query.wikidata.org/sparql?format=json&")
 // Addition paramaters like 'query' to apppend the Sparql Query.
 const params = new URLSearchParams();
-params.append('query', sparqlQuery);
+params.append('query', configStoryMap.sparqlQuery);
 // Apending the query param to the main url.
 url += params.toString()
 // This function is an helper method that is used to call the api of the wikidata.
@@ -118,9 +64,9 @@ function convertJsontoGeojson(bindings) {
     var geojson = {
         type: "FeatureCollection",
         features: [],
-        };
-    for (i = 0; i < bindings.length; i++) {
-        var featureObj =  {
+    };
+    for (var i = 0; i < bindings.length; i++) {
+        var featureObj = {
             "type": "Feature",
             "properties": bindings[i],
             "geometry": null
@@ -129,7 +75,6 @@ function convertJsontoGeojson(bindings) {
     }
     // iterating through all the bindings.
     return geojson;
-
 }
 
 /***
@@ -154,12 +99,6 @@ function reformatThebindings(newBindings) {
     return finalBindings;
 }
 
-
-// fetch('./data/leaflet/brc/map.geojson')
-//     .then(response => response.json())
-//     .then(jsonData => {
-//         generateMarkersOnMap(jsonData);
-//     });
 /***
  *  Creates the html string required for the popups in the map.
  * 
@@ -194,22 +133,11 @@ function createPopUpHtmlForBinding(binding, imagFlag = true) {
     } else {
         popUpHtml += "</h1><ul class='popup-list list-style-none'>";
     }
-    bindingKeysObject = {
-        "yearInstalled": "Year Installed:",
-        "yearRemoved": "Year Removed:",
-        "creators": "Creators:",
-        "materials": "Materials:",
-        "categories": "Categories:",
-        "neighborhoods": "Neighborhoods:",
-        "depicted": "Depicted:",
-        "commemorated": "Commemorated:",
-        "address": "Address:",
-        "workDescription": "Work Description:"
-    }
-    for (const key in bindingKeysObject) {
+
+    for (const [key, value] of Object.entries(configStoryMap.bindingKeysObject)) {
         if (binding[key]) {
             popUpHtml += "<li class='popup-item'>";
-            popUpHtml += "<strong>" + bindingKeysObject[key] + " </strong> " + binding[key];
+            popUpHtml += "<strong>" + value + " </strong> " + binding[key];
             popUpHtml += "</li>";
         }
     }
@@ -221,21 +149,25 @@ function createPopUpHtmlForBinding(binding, imagFlag = true) {
             popUpHtml += "<a href = '" + binding["work"] + "' class='more-info-span' target='_blank'>More information.... <img src='./assets/images/leaflet/brc/external-link.svg' width='10' heigth='10'></a>"
         }
     }
-
     popUpHtml += '</ul></div>';
     // return the final html that is displaid on the popup.
     return popUpHtml;
 }
 
+/***
+ *  Creates the story maps based on the bindings.
+ * 
+ * @param {Object} layer - the current layer object.
+ * @param {Object} feature - the current feature object.
+ * @param {Object} jsonData - the total geojson data.
+ */
 function createStoryMaps(layer, feature, jsonData) {
-    var imageContainerMargin = 70; // Margin + padding
-    var currentBox = 0;
+
     // This creates the contents of each chapter from the GeoJSON data. Unwanted items can be removed, and new ones can be added
     var chapter = $('<p></p>', {
         text: feature.properties['workLabel'],
         class: 'chapter-header'
     });
-
     var image = $('<img>', {
         src: '/assets/images/leaflet/brc/no-image-available.jpg',
     });
@@ -261,22 +193,20 @@ function createStoryMaps(layer, feature, jsonData) {
     var imgHolder = $('<div></div>', {
         class: 'img-holder'
     });
-
     imgHolder.append(image);
     let popUpHtml = createPopUpHtmlForBinding(feature["properties"], false);
     container.append(chapter).append(imgHolder).append(source).append(popUpHtml).append("<div class='pbt-20'></div>");
     $('#contents').append(container);
-    var i;
-    var areaTop = -300;
-    var areaBottom = 0;
-
+    var currentAreaTop = -300;
+    var currentAreaBottom = 0;
+    var currentBox = 0;
     // Calculating total height of blocks above active
-    for (i = 1; i < feature.properties['id']; i++) {
-        areaTop += $('div#container' + i).height() + imageContainerMargin;
+    for (var i = 1; i < feature.properties['id']; i++) {
+        currentAreaTop += $('div#container' + i).height() + configStoryMap.imageContainerMargin;
     }
-    areaBottom = areaTop + $('div#container' + feature.properties['id']).height();
+    currentAreaBottom = currentAreaTop + $('div#container' + feature.properties['id']).height();
     $('div#contents').scroll(function() {
-        if ($(this).scrollTop() >= areaTop && $(this).scrollTop() < areaBottom) {
+        if ($(this).scrollTop() >= currentAreaTop && $(this).scrollTop() < currentAreaBottom) {
             if (feature.properties['id'] != currentBox) {
                 currentBox = feature.properties['id'];
                 $('.image-container').removeClass("inFocus").addClass("outFocus");
@@ -288,6 +218,11 @@ function createStoryMaps(layer, feature, jsonData) {
     });
 }
 
+/***
+ *  Generated the markers on the leaflet map.
+ * 
+ * @param {Object} jsonData - the total geojson data.
+ */
 function generateMarkersOnMap(jsonData) {
     // wicket(Wkt) is a library which can parse
     // the WKTliteral (geometry data) and convert to geoJson information. 
@@ -312,11 +247,11 @@ function generateMarkersOnMap(jsonData) {
     // creating point to the map using geoJson function from the leaflet js.
     const geoJsonObj = L.geoJSON(jsonData, {
             pointToLayer: function(geoJsonPoint, latlng) {
-                var myIcon = L.icon({
-                    iconUrl: '/assets/images/leaflet/brc/marker-icon-blue.png',
-                    iconSize: [25, 41],
-                    iconAnchor: [12, 41],
-                    popupAnchor: [0, -35],
+                const myIcon = L.icon({
+                    iconUrl: configStoryMap.iconUrl,
+                    iconSize: configStoryMap.iconSize,
+                    iconAnchor: configStoryMap.iconAnchor,
+                    popupAnchor: configStoryMap.popupAnchor,
                 });
                 return L.marker(latlng, {
                     icon: myIcon
@@ -331,16 +266,16 @@ function generateMarkersOnMap(jsonData) {
         .addTo(map);
     $('#contents').append("<div class='space-at-the-bottom'><a href='#space-at-the-top'><i class='fa fa-chevron-up'></i></br><small>Top</small></a></div>");
     map.fitBounds(geoJsonObj.getBounds(), {
-        padding: [20, 20]
+        padding: configStoryMap.boundsPadding
     });
 }
 // This adds data as a new layer to the map
 function refreshLayer(data, map, coord, zoom) {
     // var dataLayer = L.geoJson(data);
     // dataLayer.addTo(map);
-    map.setView([coord[1], coord[0]], 70);
+    map.setView([coord[1], coord[0]], configStoryMap.setViewZoom);
 }
 $("div#contents").animate({
-    scrollTop: 5
+    scrollTop: configStoryMap.animateScrollTop
 });
 });
